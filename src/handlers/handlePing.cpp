e39fda30d7b8c91a2560299d7500db0b4f5a0e44
CottonKiwii <julia.wolfram@gmx.at>
2026-06-19 16:19:52 +0200

    feat: added ping and pong command

--- src/handlers/handlePing.cpp
+++ src/handlers/handlePing.cpp
@@ -1,7 +1,26 @@
-#include "Client.hpp"
-#include "Command.hpp"
+
+#include "irc.hpp"
 
 void handlePing(Client &sender, Command &command) {
-	(void)sender;
-	(void)command;
+	std::cout << "Handling PING" << std::endl;
+
+	if (command.getArgs().empty()) {
+
+		// ERR_NEEDMOREPARAMS
+		std::string response = ERR_NEEDMOREPARAMS
+			+ sender.getNick()
+			+ " :Not enough parameters\n";
+
+		sender.sendMsg(response);
+		return ;
+	}
+
+	// PING
+	std::string message = RPL_PING
+		+ sender.getNick()
+		+ " :"
+		+ command.getArgs()[0]
+		+ "\n";
+
+	sender.sendMsg(message);
 }
--- src/handlers/handlePong.cpp
+++ src/handlers/handlePong.cpp
@@ -1,8 +1,26 @@
 
-#include "Client.hpp"
-#include "Command.hpp"
+#include "irc.hpp"
 
 void handlePong(Client &sender, Command &command) {
-	(void)sender;
-	(void)command;
+	std::cout << "Handling PONG" << std::endl;
+
+	if (command.getArgs().empty()) {
+
+		// ERR_NEEDMOREPARAMS
+		std::string response = ERR_NEEDMOREPARAMS
+			+ sender.getNick()
+			+ " :Not enough parameters\n";
+
+		sender.sendMsg(response);
+		return ;
+	}
+
+	// PONG
+	std::string message = RPL_PING
+		+ sender.getNick()
+		+ " :"
+		+ command.getArgs()[0]
+		+ "\n";
+
+	sender.sendMsg(message);
 }
